f186dd50b1ca3c7ff38ca7d1ded0dded9bd41c53
Jashan Singh <116745826+JashanSin@users.noreply.github.com>
2023-03-29 14:36:11 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -51,7 +51,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=40, train
 from sklearn.linear_model import LogisticRegression
 model = LogisticRegression()
 model.fit(X_train, y_train)
+
+
+
 y_pred = model.predict(X_test)
+#prediction_probabilities = classifier.predict_proba(df)
+
+st.subheader('Prediction')
+st.write(y[y_pred])
+
+st.subheader('Class labels and their corresponding index number')
+st.write(y)
+
+
 from sklearn.metrics import accuracy_score, confusion_matrix
 accuracy = accuracy_score(y_test, y_pred)
 confusion_mat = confusion_matrix(y_test, y_pred)
